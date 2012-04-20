b113f8f1e8e57074ea718d9868ac4447484d3721
Paolo Fragomeni <hij1nx@me.com>
2012-04-20 02:28:31 -0400

    first commit\!

--- README.md
+++ README.md
@@ -1,4 +1,35 @@
 node-dir
 ========
 
-An interactive console.dir() for the terminal.
+# Synopsis
+An interactive representation of an object for the CLI similar to that of console.dir() in webkit.
+
+# Motivation
+Why? Sometimes you have a lot of data that gets dumped to the screen. It's hard to keep track of it visually. A lot of the time you are just looking for one item in a sea of data. Progressive disclosure helps.
+
+# Usage
+First, require the module. Then use `console.dir(somecode)` in your program and then use `tab` or `shift+tab` to cycle through the object's members. You can hit `space`, `return` or `enter` to expand a member. See the example below.
+
+```js
+
+console.dir = require('../dir');
+
+var stuff = { 
+  "name" : "dir", 
+  "description" : "An interactive console.dir() for the terminal.",
+  "tags" : [
+    "console",
+    "debug",
+    "debugging",
+    "json",
+    "interacive",
+    "prompt"
+  ]
+  "foo": function() { 
+    return "node tests/test.js"
+  }
+};
+
+console.dir(stuff);
+
+```
---
+++ dir.js
@@ -0,0 +1,305 @@
+
+var tty = require('tty');
+var fs = require('fs');
+var jv = exports, stack = [];
+
+var stdin = process.openStdin(); 
+tty.setRawMode(true);    
+
+var tabSize = 2;
+var filename;
+var displayed = 0;
+
+if (process.argv[0] === 'node') {
+  filename = process.argv[2];
+}
+else {
+  filename = process.argv[1];
+}
+
+var meta = [], map = [0];
+
+var selection = 1;
+var index = 0;
+
+var write = function write(s) {
+  process.stdout.write(s);
+}
+
+//
+// move the cursor upward on the screen
+//
+var up = function up(i) {
+
+  if (i > 0) {
+    while(i--) {
+      write(ws(80, true) + '\033[1A\r');
+    }
+  }
+  else {
+    write('\033[1A\r');
+  }
+};
+
+//
+// generate whitespace
+//
+var ws = function ws(i, multiplier) {
+  
+  var s = '';
+  
+  if (multiplier) {
+    i = i * tabSize;
+  }
+
+  while(i--) {
+    s += ' ';
+  }
+  return s;
+};
+
+var getType = function getType(o) {
+
+  if (typeof o === 'string' || typeof o === 'number' || 
+    typeof o === 'boolean' || typeof o === 'function') {
+    return typeof o;
+  }
+  else if (({}).toString.call(o) === '[object RegExp]') {
+    return 'regexp';
+  }
+  else if (Array.isArray(o)) {
+    return 'array';
+  }
+  else if (typeof o === 'undefined') {
+    return 'undefined';
+  }
+  else if (({}).toString.call(o) === '[object Null]') {
+    return 'null';
+  }
+  else if (({}).toString.call(o) === '[object Object]') {
+    return 'object';
+  }
+};
+
+//
+// generate a representation of the data
+//
+var indent = 0;
+var seed = -1;
+var constructMeta = function constructMeta(parentType, depth, node, itemPrefix) {
+
+  itemPrefix = itemPrefix || '';
+
+  var first = meta.length === 0;
+  var type = getType(node);
+  seed++;
+
+  switch(type) {
+    case 'string':
+    case 'number':
+    case 'boolean':
+    case 'undefined':
+    case 'regexp':
+    case 'null':
+
+      meta.push({
+        description: itemPrefix + node,
+        expanded: false,
+        displayed: first,
+        type: type,
+        depth: depth,
+        index: seed
+      });
+
+    break;
+    case 'function':
+
+      meta.push({
+        description: itemPrefix + '[Function]',
+        expanded: false,
+        displayed: first,
+        type: type,
+        depth: depth,
+        index: seed
+      });
+
+    break;
+    case 'array':
+
+      meta.push({
+        description: itemPrefix + 'Array[' + node.length + ']',
+        expanded: false,
+        displayed: first,
+        type: type,
+        depth: depth,
+        index: seed
+      });
+
+      indent++;
+      depth++;
+
+      for (var i = 0, l = node.length; i < l; i++) {
+        var description = ws(indent, true) + i + ': ';
+        constructMeta(type, depth, node[i], description);
+      }
+
+      indent--;
+
+    break;
+    case 'object':
+
+      meta.push({ 
+        description: itemPrefix + 'Object',
+        expanded: false,
+        displayed: first,
+        type: type,
+        depth: depth,
+        index: seed
+      });
+
+      indent++;
+      depth++;
+
+      for (var key in node) {
+        var description = ws(indent, true) + key + ': ';
+        constructMeta(type, depth, node[key], description);
+      }
+
+      indent--;
+
+    break;
+  }
+
+};
+
+var renderMeta = function renderMeta() {
+
+  displayed = 0;
+
+  for (var i = 0, l = meta.length; i < l; i++) {
+
+    if (meta[i].displayed === true) {
+
+      displayed++;
+
+      if (displayed === selection) {
+        write('\033[30;47m');
+      }
+
+      write(meta[i].description + '\n');
+
+      if (displayed === selection) {
+        write('\033[0m');
+      } 
+    }
+  }
+};
+
+var listener = function listener(chunk, key) {
+
+  if (key) {
+
+    var downAction = (key.name === 'tab' && !key.shift) || key.name === 'down';
+    var upAction = (key.shift && key.name === 'tab') || key.name === 'up';
+
+    if (downAction && selection < displayed) {
+
+      //
+      // get the actual index of the item from the map.
+      //
+      index = map[selection];
+
+      selection++;
+
+      up(displayed);
+      renderMeta();
+    }
+
+    if (upAction && selection > 1) {
+
+      selection--;
+      index = map[selection-1];
+
+      up(displayed);
+      renderMeta();
+    }
+
+    //
+    // if this is a toggle, the value must be of type array or object.
+    //
+    if ((key.name === 'space' || key.name === 'enter') &&
+        (meta[index].type === 'array' || meta[index].type === 'object')) {
+
+      var start = selection;
+      var stop = meta.length;
+      var next = meta[index].depth+1;
+      var started = false;
+
+      if (meta[index].type === 'string') {
+        return;
+      }
+
+      if (meta[index].expanded) {
+
+        meta[index].expanded = false;
+
+        for (var i = index, l = stop; i < l; i++) {
+
+          if (meta[i].depth >= next) {
+            meta[i].displayed = false;
+            meta[i].expanded = false;
+            started = true;
+          }
+          else if (started && meta[i].depth < next) {
+            break;
+          }
+        }
+
+      }
+      else {
+
+        meta[index].expanded = true;
+
+        for (var i = index, l = stop; i < l; i++) {
+
+          if (meta[i].depth === next) {
+            meta[i].displayed = true;
+            meta[i].expanded = false;
+            started = true;
+          }
+          else if (started && meta[i].depth < next) {
+            break;
+          }
+        }
+
+      }
+
+      map = [];
+      for (var i = 0, l = meta.length; i < l; i++) {      
+        if (meta[i].displayed === true) {
+          map.push(meta[i].index);
+        }
+      }
+
+      up(displayed);
+      renderMeta();
+    }
+
+    if (key.name === 'q' || (key.ctrl && key.name == 'c')) {
+      //write('\r\n');
+      stdin.removeListener('keypress', listener);
+      stdin.pause();
+    }
+
+  }
+};
+
+var dir = function dir(obj, options) {
+
+  constructMeta(getType(obj), 0, obj);
+  renderMeta();
+
+  stdin.on('keypress', listener);
+};
+
+module.exports = dir;
